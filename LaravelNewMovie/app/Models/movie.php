ea5436379b87f79a3e28ed82a197da22c51aa455
Benito <bbxblack@gmail.com>
2021-05-25 17:52:45 +0200

    v0.2

--- LaravelNewMovie/app/Models/movie.php
+++ LaravelNewMovie/app/Models/movie.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class movie extends Model
+class Movie extends Model
 {
-    //
+    protected $table = 'movies';
 }
