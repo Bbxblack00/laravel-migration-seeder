e1356d1b9aa9565969ef6149646d013e525e75b7
B.C <benblog2000@gmail.com>
2021-05-27 15:21:19 +0200

    v0.5

--- LaravelNewMovie/app/Http/Controllers/FilmController.php
+++ LaravelNewMovie/app/Http/Controllers/FilmController.php
@@ -7,5 +7,17 @@ use App\Models\Movies;
 
 class FilmController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+
+        return view('pages.home', compact('movies'));
+    }
+
+
+    public function show($movieid) {
+
+        $movie = Movie::find($movieid);
+
+        return view('pages.film', compact('movie'));
+    }
 }
